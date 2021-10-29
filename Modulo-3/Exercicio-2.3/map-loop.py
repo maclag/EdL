62f21331f39a4def7f2692ae7b81f160f69d6ede
Maria Clara Gomes <77252549+maclag@users.noreply.github.com>
2021-10-29 02:40:38 -0300

    Função map - loop

---
+++ Modulo-3/Exercicio-2.3/map-loop.py
@@ -0,0 +1,17 @@
+# Função map usando loop
+
+def fmap (f):
+    global l
+    original = l
+    l = []
+    for elem in original:
+        l.append(f(elem))
+    return l
+
+# Exemplo de teste:
+def square (x):
+    return x ** 2;
+
+l = [2, 4, 6, 8, 10]
+fmap(square)
+print(l) # saída: [4, 16, 36, 64, 100]
